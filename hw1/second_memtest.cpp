7dc07d22a1e1ac4fed9fff3576fff822692c14d3
Aitor Amatriain <amatriai@usc.edu>
2018-09-05 23:24:35 -0700

    Update second_memtest.cpp

--- hw1/second_memtest.cpp
+++ hw1/second_memtest.cpp
@@ -1,31 +1,50 @@
-#include <iostream>
-#include <stdlib.h>
-#include "flexCharManager.h"
-using namespace std;
+  #include <iostream>
+  #include <stdlib.h>
+  #include "simpleCharManager.h"
+  using namespace std;
 
-int main(int argc, char *argv[])
-{
-  flexCharManager simplest_mem_manager;
-  string sentence;
-  sentence = "Hello World!";
-
-  /*write driver code as described in the assignment to replace this */
-  char* c1 = simplest_mem_manager.alloc_chars(sentence.size());
-  int size_sent = sentence.size();
-  for(int i = 0; i < size_sent; i++)
+  int main(int argc, char *argv[])
   {
-  	c1[i] = sentence[i];
-  }
-  
-  //char* c2 = simplest_mem_manager.alloc_chars(3);
-  for(int i =0; i < size_sent; i++)
-  {
-  	cout << c1[i]; 
-  }
-  cout << "" << endl;
+   
+      flexCharManager simplest_mem_manager;
+      string sentence;
+      sentence = "Hello World!\n";
 
-  simplest_mem_manager.free_chars(c1);
-  
-  return 0;
-}
+      /*write driver code as described in the assignment to replace this */
+      char* c1 = simplest_mem_manager.alloc_chars(6);
+      for(int i = 0; i < 6; i++)
+      {
+        c1[i] = sentence[i];
+      }
+      
+      char* c2 = simplest_mem_manager.alloc_chars(7);
+      for(int i = 6; i < 13; i++)
+      {
+        c2[i] = sentence[i];
+      }
+      for(int i = 0;  i < 13; i++)
+      {
+        if( i < 6)
+        {
+        cout << c1[i];
+        } 
+        else
+        {
+          cout << c2[i];
+        }
+
+      }
+      
+      string sentence2 = "moon! bye\n";
+      simplest_mem_manager.free_chars(c2);
+      char* c3 = simplest_mem_manager.alloc_chars(10);
+      for( int i = 6 ; i < 16; i++ )
+      {
+        c3[i] = sentence2[i];
+      }
+      
+      return 0;
+        }
+
+
 
